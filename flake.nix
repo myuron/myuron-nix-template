9ded9e5634a07ac69186aa6f0ffb1d68e904b85a
myuron <myuron.dev@gmail.com>
2026-02-04 21:04:21 +0900

    fix:go-with-agentskills

--- flake.nix
+++ flake.nix
@@ -1,43 +1,12 @@
 {
-  description = "myuron nix templates";
+  description = "Collection of Nix flake templates for various programming languages";
 
-  inputs = {
-    nixpkg = {
-      url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
+  outputs = { self }: {
+    templates = {
+      go-with-agentskills = {
+        path = ./go-with-agentskills;
+        description = "Go development environment with agent-skills";
+      };
     };
   };
-
-  outputs =
-    { nixpkgs, flake-utils, ...}:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = with pkgs; pkgs.mkShell {
-          buildInputs = [
-            nixpkgs-fmt
-          ];
-        };
-      }) // {
-      templates =
-        let
-          list = map
-            ({ path, ... }@value: {
-              inherit value;
-              name = baseNameOf path;
-            })
-            [
-              {
-                path = ./go-with-agentskills;
-                description = "go with agent-skills flake";
-              }
-            ];
-        in
-        builtins.listToAttrs list;
-    };
 }
